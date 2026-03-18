5c42a6bae619bf786264af3ed7bd6d66dbe9aa72
Uwe L. Korn <uwe.korn@quantco.com>
2026-03-18 09:47:24 +0100

    Install upb rules

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,3 +5,6 @@ set -euxo pipefail
 mkdir -p $PREFIX/share/bazel/protobuf
 cp -r bazel $PREFIX/share/bazel/protobuf/
 rm -rf $PREFIX/share/bazel/protobuf/tests
+pushd upb
+mkdir -p $PREFIX/share/bazel/upb
+cp -r bazel $PREFIX/share/bazel/upb/
